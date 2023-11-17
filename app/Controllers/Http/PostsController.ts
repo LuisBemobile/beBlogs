eceec33baa5dbffa65c613349b01ae96f76a8036
LuisBemobile <luis@bemobile.tech>
2023-11-17 20:28:23 -0300

    feat(post):feat rout post, and verify roles

---
+++ app/Controllers/Http/PostsController.ts
@@ -0,0 +1,31 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Post from 'App/Models/Post'
+
+export default class PostsController {
+  public async store({ request, response, auth }: HttpContextContract) {
+    const { description } = request.body()
+
+    const {
+      $attributes: { id, admin },
+    } = await auth.use('api').authenticate()
+    console.log(admin)
+
+    if (!admin) {
+      return response.unauthorized({ message: 'you need administrator permission' })
+    }
+
+    try {
+      const post = await Post.create({
+        userId: id,
+        description,
+      })
+
+      return response.status(201).json({
+        message: 'Post successfully created',
+        post,
+      })
+    } catch (error) {
+      return response.status(500).json({ message: 'An error occurred while creating the post' })
+    }
+  }
+}
