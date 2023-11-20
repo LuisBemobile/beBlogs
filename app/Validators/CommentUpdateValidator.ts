9d8726d5d095bcd4a9c4dcab716b7c70668167db
LuisBemobile <luis@bemobile.tech>
2023-11-20 16:19:37 -0300

    feat(comment): feat routes put and delete

---
+++ app/Validators/CommentUpdateValidator.ts
@@ -0,0 +1,46 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { CustomMessages, rules, schema } from '@ioc:Adonis/Core/Validator'
+
+export default class CommentUpdateValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  /*
+   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
+   *
+   * For example:
+   * 1. The username must be of data type string. But then also, it should
+   *    not contain special characters or numbers.
+   *    ```
+   *     schema.string([ rules.alpha() ])
+   *    ```
+   *
+   * 2. The email must be of data type string, formatted as a valid
+   *    email. But also, not used by any other user.
+   *    ```
+   *     schema.string([
+   *       rules.email(),
+   *       rules.unique({ table: 'users', column: 'email' }),
+   *     ])
+   *    ```
+   */
+  public schema = schema.create({
+    comment: schema.string([rules.minLength(20), rules.maxLength(240)]),
+  })
+
+  /**
+   * Custom messages for validation failures. You can make use of dot notation `(.)`
+   * for targeting nested fields and array expressions `(*)` for targeting all
+   * children of an array. For example:
+   *
+   * {
+   *   'profile.username.required': 'Username is required',
+   *   'scores.*.number': 'Define scores as valid numbers'
+   * }
+   *
+   */
+  public messages: CustomMessages = {
+    'comment.required': 'The comment is mandatory',
+    'comment.minLength': 'The name must be at least 20 characters long.',
+    'comment.maxLength': 'The name must not be longer than 240 characters.',
+  }
+}
